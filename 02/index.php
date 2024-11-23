1cc2891126823af49669dea66b61910572118b1b
@javaset <vla78dan@gmail.ru>
2024-11-23 09:03:26 +0300

    firstCommit

---
+++ 02/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>Hello World!</p>
+
+<p><?php echo 'Hello World2?';?></p>
+<p><?php echo 'Hello World3?';?></p>
+<p><?php echo 'Hello World4?';?></p>
+<p><?php echo 'Hello World5?';?></p>
+
+<?php
+echo '<p>Hello World6?</p>';
+echo '<p>Hello World7?</p>';
+?>
+
+</body>
+</html>
