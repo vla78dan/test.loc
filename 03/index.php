95c61178aed4bb2d3f3b7d2e80ab464e9b6f6d8a
@javaset <vla78dan@gmail.ru>
2024-11-23 23:03:11 +0300

    less2

---
+++ 03/index.php
@@ -0,0 +1,40 @@
+<?php //phpinfo() ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lesson 3</title>
+</head>
+<body>
+
+<!-- PHP -->
+<?php
+
+/*echo '<p>Hello World1?</p>';
+echo '<p>Hello World2?</p>';
+echo '<p>Hello World2?</p>';*/
+
+echo '<p>Hello World2?</p>';
+echo '<p>Hello World2?</p>';
+echo '<p>Hello World2?</p>';
+echo '<p>Hello World2?</p>';
+echo '<p>Hello World77?</p>'; # комменарий в стиле Unix
+
+?>
+
+<?= '<p>Hello World3?</p>'; ?>
+<?php
+//echo '<p>Hello World4?</p>';
+?>
+<?php
+//echo '<p>Hello World5?</p>';
+?>
+
+
+
+</body>
+</html>
