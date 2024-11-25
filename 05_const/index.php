f2b29987b15ebe406d571f2484f798cbcb6757da
@javaset <vla78dan@gmail.ru>
2024-11-25 14:04:11 +0300

    k_05_constant_end

---
+++ 05_const/index.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(-1);
+const CONST_2 = "value 2";
+
+
+define("CONST_1", "value 1");
+
+echo CONST_1;
+
+
+
+var_dump(CONST_2);
+
+const NAME = 'Bobb';
+echo "Hello, ";
+
+//define("CONST_1", "new value 1");
+
+//$this = 'Test';
+
+//var_dump(get_defined_constants(true));
+
+
+?>
